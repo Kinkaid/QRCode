a0497ba8eda76b596a75acc251ccc53f35b67301
刘金凯 <liujinkai@kingxique.com>
2016-07-23 16:42:55 +0800

    Initial Commit

---
+++ QRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeDemo
+//
+//  Created by KinkaidLau on 16/7/23.
+//  Copyright © 2016年 KinkaidLau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeDemo
+//
+//  Created by KinkaidLau on 16/7/23.
+//  Copyright © 2016年 KinkaidLau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
